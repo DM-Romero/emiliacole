d2cba88a43c6181b4c750f6246278dc67cbfdb6a
Martín <mcflytix333@gmail.com>
2023-11-24 19:26:05 -0300

    controladores y rutas

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require ("express");
 const path = require("path");
-
+const mainRoutes = require('./routers/main.routes')
 const app = express();
 const PORT = 3030;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views", "home.html")))
-app.get("/about", (req,res) => res.sendFile(path.join(__dirname, "views", "about.html")))
-app.get("/music", (req,res) => res.sendFile(path.join(__dirname, "views", "music.html")))
-app.get("/contact", (req,res) => res.sendFile(path.join(__dirname, "views", "contact.html")))
+app.use("/", mainRoutes)
+
 
 app.listen(PORT, () => console.log("Server runing in http://localhost:" + PORT))
 
